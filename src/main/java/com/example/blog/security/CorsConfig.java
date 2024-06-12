094140315740c19b5582e2c6103f5d893a0e63f9
KangPyo <igangpyo8360@naver.com>
2024-06-12 12:35:17 +0900

    BcommitCloud

--- src/main/java/com/example/blog/security/CorsConfig.java
+++ src/main/java/com/example/blog/security/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://forumfr.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedOrigins("http://localhost:3000","http://forumfr.s3-website.ap-northeast-2.amazonaws.com","https://d3v7vlgblk96a0.cloudfront.net/login")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
