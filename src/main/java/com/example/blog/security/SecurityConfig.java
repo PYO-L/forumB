edb3837c25fca0b084999119628d15f627b9f288
KangPyo <igangpyo8360@naver.com>
2024-06-12 13:42:34 +0900

    BcommitCloud1

--- src/main/java/com/example/blog/security/SecurityConfig.java
+++ src/main/java/com/example/blog/security/SecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -66,10 +68,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.addAllowedOrigin("http://forumfr.s3-website.ap-northeast-2.amazonaws.com");
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://forumfr.s3-website.ap-northeast-2.amazonaws.com",
+                "https://d3v7vlgblk96a0.cloudfront.net"
+        ));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
